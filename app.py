c7c39530f0268a957249ddd5de3029417cf7eeaa
oxdjww <oxdjww@gmail.com>
2023-10-14 01:06:32 +0900

    fix : remove db settings

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import json
 
 from fastapi import FastAPI
 from sqlalchemy.orm import Session
-from models import Item, Base
-from db import EngineConn
+# from models import Item, Base
+# from db import EngineConn
 from typing import Union
 
 app = FastAPI()
@@ -15,10 +15,6 @@ agent_ip = "54.253.191.26"
 agent_port = "80"
 
 url = f"http://{agent_ip}:{agent_port}/"
-
-# 데이터베이스 연결 설정
-# db_conn = EngineConn()
-# Base.metadata.create_all(db_conn.engine)
 
 buffer = []
 @app.get("/")
@@ -29,23 +25,7 @@ def read_root():
 async def startup_event():
     asyncio.create_task(get_pkt_from_agent())    
     
-# 라우트: 데이터 가져오기
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int):
-    # 데이터베이스 연결과 세션 생성
-    # db = db_conn.get_session()
-    
-    # 데이터베이스에서 아이템 검색
-    # db_item = db.query(Item).filter(Item.id == item_id).first()
-    
-    # 데이터베이스 세션 종료
-    # db.close()
-    
-    # 아이템을 딕셔너리 형태로 반환
-    # if db_item:
-    #     return db_item.to_dict()
-    # else:
-    #     return {"error": "Item not found"}
+
 
 @app.get("/summary/security")
 def return_traffic_log():
@@ -88,3 +68,25 @@ async def get_pkt_from_agent():
         except Exception as e:
             print({"error": str(e)})
         await asyncio.sleep(1)
+
+# 데이터베이스 연결 설정
+# db_conn = EngineConn()
+# Base.metadata.create_all(db_conn.engine)
+
+# 라우트: 데이터 가져오기
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int):
+    # 데이터베이스 연결과 세션 생성
+    # db = db_conn.get_session()
+    
+    # 데이터베이스에서 아이템 검색
+    # db_item = db.query(Item).filter(Item.id == item_id).first()
+    
+    # 데이터베이스 세션 종료
+    # db.close()
+    
+    # 아이템을 딕셔너리 형태로 반환
+    # if db_item:
+    #     return db_item.to_dict()
+    # else:
+    #     return {"error": "Item not found"}
--- models.py
+++ models.py
@@ -1,18 +1,18 @@
-# models.py
+# # models.py
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+# Base = declarative_base()
 
-class Item(Base):
-    __tablename__ = 'test'  # 데이터베이스 테이블 이름
+# class Item(Base):
+#     __tablename__ = 'test'  # 데이터베이스 테이블 이름
     
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, index=True)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-        }
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#         }
