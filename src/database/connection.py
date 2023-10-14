0a6ed39c3f9c12444a1d2de5e5745444fa2ee989
oxdjww <oxdjww@gmail.com>
2023-10-14 10:33:49 +0900

    fix : comment out DB info

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,16 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# 로컬 디비 테스트 (임시)
-DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/ploio_db"
-
-engine = create_engine(DATABASE_URL)
-SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def get_ploio_db():
-    session = SessionFactory()
-    try:
-        yield session
-    finally:
-        session.close()
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+#
+# # 로컬 디비 테스트 (임시)
+# DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/ploio_db"
+#
+# engine = create_engine(DATABASE_URL)
+# SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+#
+# def get_ploio_db():
+#     session = SessionFactory()
+#     try:
+#         yield session
+#     finally:
+#         session.close()
--- src/database/repository.py
+++ src/database/repository.py
@@ -1,16 +1,16 @@
-from typing import List
-
-from fastapi import Depends
-from sqlalchemy import select, delete
-from sqlalchemy.orm import Session
-
-from database.connection import get_ploio_db
-from database.orm import test
-
-
-class PloioRepository:
-    def __init__(self, session: Session = Depends(get_ploio_db)):
-        self.session = session
-
-    def get_test_data(self) -> List[test]:
-        return list(self.session.scalars(select(test)))
+# from typing import List
+#
+# from fastapi import Depends
+# from sqlalchemy import select, delete
+# from sqlalchemy.orm import Session
+#
+# from database.connection import get_ploio_db
+# from database.orm import test
+#
+#
+# class PloioRepository:
+#     def __init__(self, session: Session = Depends(get_ploio_db)):
+#         self.session = session
+#
+#     def get_test_data(self) -> List[test]:
+#         return list(self.session.scalars(select(test)))
