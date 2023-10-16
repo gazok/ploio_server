679bbcbadf54e834aac35fe0ed143f490a356814
oxdjww <oxdjww@gmail.com>
2023-10-16 18:43:24 +0900

    feat : add sig, ver, cnt parse

--- src/service/summary.py
+++ src/service/summary.py
@@ -1,4 +1,7 @@
+import json
+
 import requests
+import struct
 
 agent_ip = "54.253.191.26"
 agent_port = "8080"
@@ -12,8 +15,14 @@ class AgentService:
         response = requests.get(f"{self.agent_url}")
         return response.content
 
-    def parse_data(self, data):
-        # 데이터 파싱 로직 작성
-        parsed_data = {}
-        # 데이터 파싱 로직 작성
-        return parsed_data
+    def parse_data(self, bitstream):
+        signature, version, count_of_log = struct.unpack('4s4si', bitstream)
+        # JSON 데이터 생성
+        data = {
+            "signature": signature.decode('utf-8'),  # 바이트를 문자열로 디코딩
+            "version": version.decode('utf-8'),  # 바이트를 문자열로 디코딩
+            "count_of_log": count_of_log
+        }
+        with open("tmp/parsed_data", 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        return data
