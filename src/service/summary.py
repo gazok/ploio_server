5fcc6f7b2b604fb3c97222363cb47628ce6620b3
oxdjww <oxdjww@gmail.com>
2023-10-17 16:44:43 +0900

    test : parser test

--- src/service/summary.py
+++ src/service/summary.py
@@ -3,6 +3,7 @@ import asyncio
 import json
 import requests
 import struct
+import sys
 
 from collections import deque
 
@@ -19,10 +20,15 @@ async def fetching_process():
     while True:
         print('while')
         agent_service = AgentService(agent_ip, agent_port)
-        response_content = await agent_service.send_http_get_request()
-        data = agent_service.parse_data(response_content)
-        print(data)
+        # response_content = await agent_service.send_http_get_request()
+        print("here")
+        # data = agent_service.parse_data(response_content)
+        with open("tmp/sample.bin","rb") as file:
+            bitstream = file.read()
+        data = agent_service.parse_data(bitstream)
+        # print(data)
         parsed_data.append(data) # 리스트에 임시 저장. 근데 일정량이 차면 비워줘야 하는데 어떻게?
+        await asyncio.sleep(2)
 
 
 class AgentService:
@@ -39,14 +45,43 @@ class AgentService:
     # fetching process에서 쓰는 멤버함수.
     # req로 받아온 reply data를 파싱하는 로직
     def parse_data(self, bitstream):
-        signature, version, count_of_log = struct.unpack('4s4si', bitstream)
-        # JSON 데이터 생성
-        # count만큼 52byte 데이터를 추가적으로 파싱해야 함
-        data = {
-            "signature": signature.decode('utf-8'),  # 바이트를 문자열로 디코딩
-            "version": version.decode('utf-8'),  # 바이트를 문자열로 디코딩
-            "count_of_log": count_of_log
+
+        log_data = {
+            "data": []
         }
-        with open("tmp/parsed_data", 'w') as json_file:
-            json.dump(data, json_file, indent=4)
+        signature, version, count_of_log = struct.unpack('4s4si', bitstream[:12])
+
+
+        print(bitstream[:12])
+        log_data["signature"] = signature.decode('utf-8')  # 바이트를 문자열로 디코딩
+        log_data["version"] = version.decode('utf-8')  # 바이트를 문자열로 디코딩
+        log_data["count_of_log"] = count_of_log
+        print(sys.getsizeof(signature))
+        print(sys.getsizeof(version))
+        print(sys.getsizeof(count_of_log))
+        print("sig : ", signature)
+        print("ver : " , version)
+        print("cnt : " , count_of_log)
+        print(log_data)
+        offset = 12  # 이미 읽은 바이트 수
+
+        for i in range(count_of_log):
+            data = {}
+            unix_epoch, protocol, sip, dip, sport, dport, size = struct.unpack('qi16s16shhi', bitstream[offset:offset+52])
+            
+            data["unix_epoch"] = unix_epoch
+            data["protocol"] = protocol
+            data["sip"] = sip.decode('utf-8')
+            data["dip"] = dip.decode('utf-8')
+            data["sport"] = sport
+            data["dport"] = dport
+            data["size"] = size
+
+            log_data["data"].append(data)
+            print(data)
+            offset += 52  # 다음 로그 항목으로 이동
+
+        with open("tmp/parsed_data.json", 'w') as json_file:
+            json.dump(log_data, json_file, indent=4)
+
         return data
