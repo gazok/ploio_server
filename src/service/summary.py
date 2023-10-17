1dd464ca0f9d50bee8af016ceb0e2f3aeaa5c7d6
oxdjww <oxdjww@gmail.com>
2023-10-17 18:26:38 +0900

    test : sample parser w/ sample.bin

--- src/service/summary.py
+++ src/service/summary.py
@@ -15,19 +15,17 @@ agent_port = "8080"
 parsed_data = deque(maxlen=100)  # 데이터를 저장할 큐, 최대 크기를 10으로 설정
 
 async def fetching_process():
-    print('helee')
     # 반복적으로 req를 보내고, reply를 파싱해서 공유변수 parsed_data에 저장
     while True:
-        print('while')
         agent_service = AgentService(agent_ip, agent_port)
         # response_content = await agent_service.send_http_get_request()
-        print("here")
         # data = agent_service.parse_data(response_content)
         with open("tmp/sample.bin","rb") as file:
             bitstream = file.read()
         data = agent_service.parse_data(bitstream)
         # print(data)
         parsed_data.append(data) # 리스트에 임시 저장. 근데 일정량이 차면 비워줘야 하는데 어떻게?
+
         await asyncio.sleep(2)
 
 
@@ -49,25 +47,19 @@ class AgentService:
         log_data = {
             "data": []
         }
-        signature, version, count_of_log = struct.unpack('4s4si', bitstream[:12])
+        signature, version, count_of_log = struct.unpack('>4s4si', bitstream[:12])
 
 
         print(bitstream[:12])
         log_data["signature"] = signature.decode('utf-8')  # 바이트를 문자열로 디코딩
         log_data["version"] = version.decode('utf-8')  # 바이트를 문자열로 디코딩
         log_data["count_of_log"] = count_of_log
-        print(sys.getsizeof(signature))
-        print(sys.getsizeof(version))
-        print(sys.getsizeof(count_of_log))
-        print("sig : ", signature)
-        print("ver : " , version)
-        print("cnt : " , count_of_log)
         print(log_data)
         offset = 12  # 이미 읽은 바이트 수
 
         for i in range(count_of_log):
             data = {}
-            unix_epoch, protocol, sip, dip, sport, dport, size = struct.unpack('qi16s16shhi', bitstream[offset:offset+52])
+            unix_epoch, protocol, sip, dip, sport, dport, size = struct.unpack('>qi16s16shhi', bitstream[offset:offset+52])
             
             data["unix_epoch"] = unix_epoch
             data["protocol"] = protocol
