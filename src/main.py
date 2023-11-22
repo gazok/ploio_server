e7055f179794674059d023dcfda6393420366c59
oxdjww <oxdjww@gmail.com>
2023-11-22 21:39:58 +0900

    del : 레거시 코드 제거

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from api import user, summary, dbtest
 from fastapi.middleware.cors import CORSMiddleware
 
-from service.summary import agent_ip, agent_port, AgentService, fetching_process
-
 import requests
 import asyncio
 
@@ -11,10 +9,7 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(summary.router)
 # app.include_router(dbtest.router)
-origins = [
-    "http://localhost/",
-    "http://localhost:3000/"
-]
+origins = ["http://localhost/", "http://localhost:3000/"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -23,12 +18,7 @@ app.add_middleware(
     allow_headers=[""],
 )
 
+
 @app.get("/")
 def api_check_handler():
     return {"hello": "world"}
-
-
-@app.on_event("startup")
-async def startup_event():
-    # fetching process를 fastapi 서버가 올라가는 동시에 실행
-    asyncio.create_task(fetching_process())
