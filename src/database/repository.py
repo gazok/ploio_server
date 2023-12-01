2f5e1434793655e5f05f39949ac0eafe42f496e7
oxdjww <oxdjww@gmail.com>
2023-12-01 17:54:28 +0900

    함수 분리 및 .get() 예외 처리 w/ default val

--- src/database/repository.py
+++ src/database/repository.py
@@ -72,37 +72,33 @@ class PloioRepository:
         # packet_sample = {
         #     "data": [
         #         {
-        #             "packet_id": "pod-123",
-        #             "src_pod": "default:front-end3",
-        #             "dst_pod": "default:api-server",
+        #             "packet_id": "pkt-1",
+        #             "src_pod": "default:pod-1",
+        #             "dst_pod": "default:api-server1",
         #             "timestamp": "12341234",
         #             "data_len": 1024,
-        #         }
+        #         },
+        #         {
+        #             "packet_id": "pkt-2",
+        #             "src_pod": "default:pod-2",
+        #             "dst_pod": "default:api-server2",
+        #             "timestamp": "456564",
+        #             "data_len": 512,
+        #         },
         #     ]
         # }
         created_notices = []
-        for log_id, log_entry in log_list.items():
-            code = log_entry.get("Code")
-            if code in ["Warning", "Fail", "Critical"]:
-                for ref in log_entry.get("Refs", []):
-                    if ref.get("Source") == "Packet":
-                        packet_id = ref.get("Identifier")
-                        for packet in packet_data.data:
-                            notice = Notice(
-                                packet_id=packet_id,
-                                src_pod=packet["src_pod"],
-                                dst_pod=packet["dst_pod"],
-                                timestamp=packet["timestamp"],
-                                data_len=packet["data_len"],
-                                danger_degree=code,
-                                danger_message=log_entry.get("Message", ""),
-                            )
-                            created_notices.append(notice)
-                            db.add(notice)
-                        db.commit()
 
-                        for notice in created_notices:
-                            db.refresh(notice)
+        for log_id, log_entry in log_list.items():
+            code = log_entry.get("Code", "Trace")
+
+            if code in ["Warning", "Fail", "Critical"]:
+                self.process_notices_with_log(
+                    log_entry, packet_data, created_notices, db
+                )
+
+        db.commit()
+        self.refresh_notices(created_notices, db)
 
         for log_pod in created_notices:
             for saved_pod in pod_data.pods:
@@ -111,3 +107,42 @@ class PloioRepository:
                     saved_pod["danger_message"] = log_pod.danger_message
 
         return created_notices
+
+    def process_notices_with_log(self, log_entry, packet_data, created_notices, db):
+        for ref in self.get_packet_refs(log_entry):
+            malicious_packet_id = self.get_packet_identifier(ref)
+
+            packet_info = self.find_packet_info(packet_data, malicious_packet_id)
+            if packet_info:
+                notice = self.create_notice_from_packet(ref, packet_info, log_entry)
+                self.save_notice_to_database(notice, created_notices, db)
+
+    def get_packet_refs(self, log_entry):
+        return log_entry.get("Refs", [])
+
+    def get_packet_identifier(self, ref):
+        return ref.get("Identifier", "None")
+
+    def find_packet_info(self, packet_data, malicious_packet_id):
+        for packet in packet_data.data:
+            if packet["packet_id"] == malicious_packet_id:
+                return packet
+
+    def create_notice_from_packet(self, ref, packet_info, log_entry):
+        return Notice(
+            packet_id=ref.get("Identifier", "None"),
+            src_pod=packet_info["src_pod"],
+            dst_pod=packet_info["dst_pod"],
+            timestamp=packet_info["timestamp"],
+            data_len=packet_info["data_len"],
+            danger_degree=log_entry.get("Code", "Trace"),
+            danger_message=log_entry.get("Message", "Trace symbol/mark"),
+        )
+
+    def save_notice_to_database(self, notice, created_notices, db):
+        created_notices.append(notice)
+        db.add(notice)
+
+    def refresh_notices(self, created_notices, db):
+        for notice in created_notices:
+            db.refresh(notice)
