d28e291b4c6efda97581cae63afd8f3d6acd5575
oxdjww <oxdjww@gmail.com>
2023-12-04 19:56:50 +0900

    refactor : column str->timestamp

--- src/model/domain/packet.py
+++ src/model/domain/packet.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 from typing import List
-
+from datetime import datetime
 
 class PacketItem(BaseModel):
     packet_id: str
     src_pod: str
     dst_pod: str
-    timestamp: str
+    timestamp: datetime
     data_len: int
 
 
